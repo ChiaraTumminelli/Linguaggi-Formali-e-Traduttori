84b973190c44de89b0879f2171b9d187ff419563
Chiara Tumminelli <chiara.tumminelli@edu.unito.it>
2022-06-11 01:59:41 +0200

    Add files via upload

---
+++ Exercise_5/NumberTok.java
@@ -0,0 +1,11 @@
+package Exercise_5;
+
+public class NumberTok extends Token {
+
+    // ... To complete ...
+    public int num;
+    public NumberTok (int tag, int s) { super(tag); num = s; }
+    public String toString(){ return "<" + tag + ", " + num + ">"; }
+
+  
+}
